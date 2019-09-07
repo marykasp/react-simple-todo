f53d0448a22803707b50d1a386a100d6709afb92
Mary Kasparian <kasparian.mary@gmail.com>
2019-09-07 08:33:41 -0500

    fetched todos from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -6,29 +6,19 @@ import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
 import uuid from 'uuid';
+import axios from 'axios';
 
 
 
 
   class App extends React.Component {
     state = {
-      todos: [
-        {
-          id: uuid.v4(),
-          title: 'Take out the trash',
-          completed: false
-        },
-        {
-          id: uuid.v4(),
-          title: 'Dinner with spouse',
-          completed: false
-        },
-        {
-          id: uuid.v4(),
-          title: 'Meeting with boss',
-          completed: false
-        }
-      ]
+      todos: []
+    }
+
+    componentDidMount() {
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(res => this.setState({ todos: res.data }))
     }
 
     // Toggle Complete
